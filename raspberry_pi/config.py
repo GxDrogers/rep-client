d17b4137a3bbb1a076a2002390838a1ea6e5a1c4
C0deDev!l <89889309+GxDrogers@users.noreply.github.com>
2025-04-06 21:48:21 +0530

    Delete raspberry_pi directory

--- raspberry_pi/config.py
+++
@@ -1,6 +0,0 @@
-PI_IP = "192.168.1.100"  # Replace with your Raspberry Pi's IP
-SERVER_IP = "192.168.1.200"  # Replace with your PC/laptop's IP
-VIDEO_PORT = 8000
-AUDIO_PORT = 8001
-COMMAND_PORT = 8002
-DB_PATH = "./server/students.db"
